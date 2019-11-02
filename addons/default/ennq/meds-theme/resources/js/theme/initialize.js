61d91c8801ec49e68e9b365e1c6306d187a6e6ed
ennaque <volkov.enn@gmail.com>
2019-11-03 02:07:03 +0300

    finish 4 seeder, add datatables

--- addons/default/ennq/meds-theme/resources/js/theme/initialize.js
+++ addons/default/ennq/meds-theme/resources/js/theme/initialize.js
@@ -1,7 +1,17 @@
 (function (window, document) {
 
     $(document).ready(function() {
-        $('#test-table').DataTable();
+        $('#meds-table').DataTable({
+            responsive: true,
+            ordering: false,
+            "pageLength": 112,
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Russian.json"
+            },
+            "scrollX": true,
+            "bAutoWidth": true
+        });
     } );
-    
+
+
 })(window, document);
